a20e10937f96a3cab020c4a0763187c081ae3fcb
Alex <alexandre.p.a.94@gmail.com>
2026-08-25 22:13:34 +0200

    Update ejercicio1.js

---
+++ ejercicio1.js
@@ -0,0 +1,11 @@
+// variables
+let miSuperheroeFavorito = "Hulk";
+let numeroFavorito = 50;
+let altura = 5;
+let longitud = 10;
+let suma = altura + longitud;
+
+//lo que muestra en consola
+console.log("Superhéroe:", miSuperheroeFavorito);
+console.log("Número favorito:", numeroFavorito);
+console.log("Altura:", altura);
