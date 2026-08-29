2576cbb8b40739f6c9fffadb581e4147ce39130b
Alex <alexandre.p.a.94@gmail.com>
2026-08-29 22:47:40 +0200

    ejercicio21

---
+++ ejercicio21.js
@@ -0,0 +1,21 @@
+const users = [
+  { name: "Tony", years: 43 },
+  { name: "Peter", years: 18 },
+  { name: "Natasha", years: 14 },
+  { name: "Bruce", years: 32 },
+  { name: "Khamala", years: 16 }
+];
+
+const minors = [];
+const adults = [];
+
+for (const user of users) {
+  if (user.years < 18) {
+    minors.push(user.name);
+  } else {
+    adults.push(user.name);
+  }
+}
+
+console.log("Usuarios menores de edad:", minors);
+console.log("Usuarios mayores de edad:", adults);
