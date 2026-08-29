921b6158da500b7a20250f4521286f661c05790d
Alex <alexandre.p.a.94@gmail.com>
2026-08-29 18:45:29 +0200

    ejercicio12.js

---
+++ ejercicio12.js
@@ -0,0 +1,15 @@
+const duplicates = [  'sushi',  'pizza',  'burger',  'potatoe',  'pasta',  'ice-cream',  'pizza',  'chicken',  'onion rings',  'pasta',  'soda'];
+
+function removeDuplicates(list) {
+  const uniqueList = [];
+
+  for (let i = 0; i < list.length; i++) {
+    if (!uniqueList.includes(list[i])) {
+      uniqueList.push(list[i]);
+    }
+  }
+
+  return uniqueList;
+}
+
+console.log(removeDuplicates(duplicates));
